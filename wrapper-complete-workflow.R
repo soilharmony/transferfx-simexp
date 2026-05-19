7b07c1f0e28aed1d7a2fe9698153895c3b7a067b
Ben Serrien <ben.serrien@inbo.be>
2026-05-19 12:17:11 +0200

    docx

--- wrapper-complete-workflow.R
+++ wrapper-complete-workflow.R
@@ -1,13 +1,22 @@
 # wrapper around complete workflow
 
 #' single_rep
-single_rep <- function(data_train, data_test, model, sigma_mex) {
+#' 
+#' @description
+#' A short description...
+#' 
+#' @param data_train Training dataset
+#' @param data_test Independent test dataset for validation
+#' @param model Name of the model ("linreg", "eivreg1", "eivreg2")
+#' @param sdmex Known value of the SD of the measurement error in x (for use in "eivreg1")
+#' 
+single_rep <- function(data_train, data_test, model, sdmex) {
   
   # (1) fit model
   model <- switch(
     model,
     linreg  = fit_linreg(data_train),
-    eivreg1 = fit_eiv1(data_train, sigma_mex),
+    eivreg1 = fit_eiv1(data_train, sdmex),
     eivreg2 = fit_eiv2(data_train)
   )
   
