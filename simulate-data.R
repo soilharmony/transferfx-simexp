7e9b817b2855c16cee2bf5b7afe959e971772e3b
Ben Serrien <ben.serrien@inbo.be>
2026-05-19 12:16:50 +0200

    switching to sigma ipv sigma^2

--- simulate-data.R
+++ simulate-data.R
@@ -9,41 +9,41 @@
 #' @param alpha Intercept of the linear relation between latent X and Y (default 0).
 #' @param beta Slope of the linear relation between latent X and Y (default 1).
 #' @param mu_x Mean of latent X distribution (default 0).
-#' @param sigma2_x Variance of latent X distribution (default 1).
-#' @param ratio_varmey_varmex Ratio of measurement error variance in Y vs. measurement error variance in X (default 1 as in Deming regression).
-#' @param ratio_varmex_varx Ratio of measurement error variance in X vs. variance in latent X.
+#' @param sigma_x SD of latent X distribution (default 1).
+#' @param ratio_sdmey_sdmex Ratio between SD of measurement error in Y and SD of measurement error in X (default 1 as in Deming regression).
+#' @param ratio_sdmex_sigmax Ratio between SD of measurement error in X and SD of latent X.
 #' @param tails Character, should be one of "normal" (normally distributed measurement errors), "t_df3" (t-distribution with 3 dof).
 #'
 #' @export
 sim_data <- function(
     N,
-    ratio_varmex_varx,
+    ratio_sdmex_sigmax,
     tails,
     alpha = 0,
     beta = 1,
     mu_x = 0,
-    sigma2_x = 1,
-    ratio_varmey_varmex = 1
+    sigma_x = 1,
+    ratio_sdmey_sdmex = 1
 ) {
 
   # latent predictor
-  x_true <- rnorm(N, mu_x, sqrt(sigma2_x))
+  x_true <- rnorm(N, mu_x, sigma_x)
   # observed predictor
-  sigma2_mex <- ratio_varmex_varx * sigma2_x
+  sdmex <- ratio_sdmex_sigmax * sigma_x
   x_obs <- switch(
     tails,
-    normal = x_true + rnorm(N, 0, sqrt(sigma2_mex)),
-    t_df3  = x_true + extraDistr::rlst(N, 3, 0, sqrt(sigma2_mex))
+    normal = x_true + rnorm(N, 0, sdmex),
+    t_df3  = x_true + extraDistr::rlst(N, 3, 0, sdmex)
   )
 
   # latent outcome
   y_true <- alpha + beta * x_true
   # observed outcome
-  sigma2 <- ratio_varmey_varmex * sigma2_mex
+  sdmey <- ratio_sdmey_sdmex * sdmex
   y_obs <- switch(
     tails,
-    normal = y_true + rnorm(N, 0, sqrt(sigma2)),
-    t_df3  = y_true + extraDistr::rlst(N, 3, 0, sqrt(sigma2))
+    normal = y_true + rnorm(N, 0, sdmey),
+    t_df3  = y_true + extraDistr::rlst(N, 3, 0, sdmey)
   )
 
   # return data.frame with observed and latent variables
