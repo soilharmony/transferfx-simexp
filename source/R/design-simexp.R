a8592308d98ec02867b568b8c704ee0f07948d59
Ben Serrien <ben.serrien@inbo.be>
2026-06-16 08:27:37 +0200

    updated pipeline with more scenarios

--- source/R/design-simexp.R
+++ source/R/design-simexp.R
@@ -3,8 +3,8 @@
 # parameter choices of the simulation experiment
 # should match the arguments of the function sim_data
 simexp_design <- expand_grid(
-  sample_size        = c(100, 200, 500),
-  ratio_sdmex_sigmax = c(0.05, 0.1, 0.2),
+  sample_size        = c(100, 200, 500, 1000),
+  ratio_sdmex_sigmax = c(0.01, 0.05, 0.1, 0.2),
   ratio_sdmey_sdmex  = 1,
   tails              = c("normal", "tdf3")
 ) %>%
